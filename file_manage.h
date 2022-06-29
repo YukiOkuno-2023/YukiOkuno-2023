9991b2a1270109e36978e22ff5e1170381582404
YukiOkuno-2023 <101882416+YukiOkuno-2023@users.noreply.github.com>
2022-06-29 17:32:52 +0900

    Delete file_manage.h

--- file_manage.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "main.h"
-
-// ŠÖ”’è‹`
-void fol_manage(void);					//FolderŠÇ—
-void copy_file(char* src, char* dst);	//File Copy
-void data_update(void);					//DataXV
-void compare_data(void);				//Data”äŠr
